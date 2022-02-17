b1839ce7b4b09393c511683b781f810277c3f231
Junaid <junaid.khokhar@vaudience.net>
2022-02-17 17:42:55 +0500

    navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
 import 'package:periscope/models/bottom_bar.dart';
-import 'package:flutter/cupertino.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,117 +10,68 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-enum BottomIcons { Home, Favorite, share, smiles, Account }
-
 class _HomeScreenState extends State<HomeScreen> {
-  BottomIcons bottomIcons = BottomIcons.Home;
+  int index = 0;
+  final List<Widget> _children = [
+    const Center(
+      child: Text('Center 1st of the page'),
+    ),
+    const Center(
+      child: Text('Center 2nd of the page'),
+    ),
+    const Center(
+      child: Text('Center 3rd of the page'),
+    ),
+    const Center(
+      child: Text('Center 4th of the page'),
+    ),
+    const Center(
+      child: Text('Center 5th of the page'),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: <Widget>[
-          bottomIcons == BottomIcons.Home
-              ? const Center(
-                  child: Text(
-                    "this is home page",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                )
-              : Container(),
-          bottomIcons == BottomIcons.Favorite
-              ? const Center(
-                  child: Text(
-                    "Hi, this is favorite page",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                )
-              : Container(),
-          bottomIcons == BottomIcons.smiles
-              ? const Center(
-                  child: Text(
-              "Send smiles",
-              style: TextStyle(fontSize: 18),
-               ),
-               )
-              : Container(),
-          bottomIcons == BottomIcons.share
-              ? const Center(
-                  child: Text(
-                    "share",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                )
-              : Container(),
-          bottomIcons == BottomIcons.Account
-              ? const Center(
-                  child: Text(
-                    "account page",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                )
-              : Container(),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              padding: EdgeInsets.only(left: 24, right: 24, bottom: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  BottomBar(
-                      onPressed: () {
-                        setState(() {
-                          bottomIcons = BottomIcons.Home;
-                        });
-                      },
-                      bottomIcons:
-                          bottomIcons == BottomIcons.Home ? true : false,
-                      icons: EvaIcons.messageCircle,
-                      text: "chat"),
-                  BottomBar(
-                      onPressed: () {
-                        setState(() {
-                          bottomIcons = BottomIcons.Favorite;
-                        });
-                      },
-                      bottomIcons:
-                          bottomIcons == BottomIcons.Favorite ? true : false,
-                      icons: EvaIcons.heartOutline,
-                      text: "Sent Love"),
-                  BottomBar(
-                      onPressed: () {
-                        setState(() {
-                          bottomIcons = BottomIcons.smiles;
-                        });
-                      },
-                      bottomIcons:
-                      bottomIcons == BottomIcons.smiles ? true : false,
-                      icons: EvaIcons.smilingFace,
-                      text: "Sent smiles"),
-                  BottomBar(
-                      onPressed: () {
-                        setState(() {
-                          bottomIcons = BottomIcons.share;
-                        });
-                      },
-                      bottomIcons:
-                          bottomIcons == BottomIcons.share ? true : false,
-                      icons: EvaIcons.share,
-                      text: "share"),
-                  BottomBar(
-                      onPressed: () {
-                        setState(() {
-                          bottomIcons = BottomIcons.Account;
-                        });
-                      },
-                      bottomIcons:
-                          bottomIcons == BottomIcons.Account ? true : false,
-                      icons: EvaIcons.personOutline,
-                      text: "Account"),
-                ],
-              ),
+          Expanded(child: _children[index]),
+        ],
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: index,
+        showElevation: true,
+        itemCornerRadius: 24,
+        curve: Curves.easeIn,
+
+        onItemSelected: (ind) => setState(() => index = ind),
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+            icon: Icon(EvaIcons.messageCircle),
+            title: Text('Home'),
+            activeColor: Colors.red,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.people),
+            title: Text('Users'),
+            activeColor: Colors.purpleAccent,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.message),
+            title: Text(
+              'Messages test for mes teset test test ',
             ),
-          )
+            activeColor: Colors.pink,
+            textAlign: TextAlign.center,
+          ),
+          BottomNavyBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+            activeColor: Colors.blue,
+            textAlign: TextAlign.center,
+          ),
         ],
       ),
     );
